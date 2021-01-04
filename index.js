c7d1deff0f1de25829576753e7a744abd8ab096f
Manny2592 <51476879+Manny2592@users.noreply.github.com>
2021-01-03 20:04:03 -0500

    First Commit

---
+++ index.js
@@ -0,0 +1,4 @@
+var image=document.querySelector(".abc");
+image.addEventListener("mouseover",function (){image.src = "images/circle-cropped-Emanue.png"});
+image.addEventListener("click",function (){image.src = "images/circle-cropped-Emanue.png"});
+image.addEventListener("mouseout",function (){image.src = "images/emanuel.png"});
